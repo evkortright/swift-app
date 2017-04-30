c6f33f5fba17e38d8a39a072f6cf87387f3827a3
Enrique V. Kortright <enrique.kortright@us.ibm.com>
2017-04-29 21:29:13 -0500

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Enrique V. Kortright on 4/29/17.
+//  Copyright © 2017 Enrique V. Kortright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
